4ebd9f4bcf6e093cbf3037873647f1c9c97276e7
KYZZMA <sanya.alex.kuz@mail.ru>
2023-03-04 15:13:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/notes/NotesClick.java
@@ -0,0 +1,14 @@
+package com.example.notes;
+
+import androidx.cardview.widget.CardView;
+
+import com.example.notes.Models.Notes;
+
+public interface NotesClick {
+
+    void onClick (Notes notes);
+    void onLongClick(Notes notes, CardView cardView);
+
+
+
+}
